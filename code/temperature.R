f29d2f6c39d4a82bd2e590be98726680654dab06
D4CAE <data4climateactionedinburgh@gmail.com>
2025-02-16 20:46:23 +0000

    not working - ncvar_get cannot find the variables I can see in the environment pane

--- code/temperature.R
+++ code/temperature.R
@@ -13,3 +13,7 @@ data_filenm_temprtr <- "tas_hadukgrid_uk_1km_mon_202403.nc"
 
 # import one month's data
 temperature_data <- nc_open(here(data_dir_name, data_subdir, data_filenm_temprtr))
+
+# call ncvar_get() to access data from a varbl
+temperature_data |> 
+  ncvar_get(varid = "format")
