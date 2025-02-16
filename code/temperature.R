a46f9a6a312c15a50bff92ad26e4bf7beaefebc5
D4CAE <data4climateactionedinburgh@gmail.com>
2025-02-16 21:00:19 +0000

    print call on the ncdf object

--- code/temperature.R
+++ code/temperature.R
@@ -14,6 +14,9 @@ data_filenm_temprtr <- "tas_hadukgrid_uk_1km_mon_202403.nc"
 # import one month's data
 temperature_data <- nc_open(here(data_dir_name, data_subdir, data_filenm_temprtr))
 
+
 # call ncvar_get() to access data from a varbl
 temperature_data |> 
   ncvar_get(varid = "format")
+
+print(temperature_data)
