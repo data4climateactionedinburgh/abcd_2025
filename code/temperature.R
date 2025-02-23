32851a3e01962a70685db631099f76081c17db1f
D4CAE <data4climateactionedinburgh@gmail.com>
2025-02-23 11:53:34 +0000

    separate folder for open data

--- code/temperature.R
+++ code/temperature.R
@@ -7,7 +7,10 @@ library(ncdf4.helpers)
 library(tidync)
 library(here)
 
-data_dir_name <- "data" 
+# Data from Met Office's HadUK Grid, released by Met Office under the OGL 
+# - see README.md for further details. 
+
+data_dir_name <- "open_data" 
 data_subdir <- "temperature_HadUK_Grid"
 # Use March as an example data file
 data_filenm_temprtr <- "tas_hadukgrid_uk_1km_mon_202403.nc"
