e70685ea9ec3105850927632fca4b9d603d784aa
D4CAE <data4climateactionedinburgh@gmail.com>
2025-03-23 10:55:22 +0000

    puzzling plot of cycling data

--- code/active_travel.R
+++ code/active_travel.R
@@ -11,9 +11,28 @@ CEC_daily_cycling_COD_alldates <- read_csv(
                                           validCountsDetected = col_character(), 
                                           withinExpectedLimits = col_character()))
 # data cleaning
-# remove outliers
+# remove outliers 
 daily_cycling <- CEC_daily_cycling_COD_alldates |>
-  filter(!is.na(withinExpectedLimits))
+  filter(!is.na(withinExpectedLimits))|> # approx 30,000 rows from 200,000
+  rename(cycles_count = count) # rename to avoid confusion with R keyword 'count'
+
+# Check dates are consistent
+daily_cycling <- daily_cycling |>
+  mutate(period_measured = as.Date(endTime) - as.Date(startTime))
+
+# The following check confirms that the gap between start and end time
+# is always 1 day. 
+# unique(daily_cycling[["period_measured"]])
+# [1] 1
+
+# Plot
+cycling_plot <- daily_cycling |>
+  select(endTime, cycles_count)|>
+  mutate(CountDate = as.Date(endTime)) |>
+  group_by(CountDate) |>
+  summarise(sum(cycles_count)) 
+
+  plot(cycling_plot)
 
 
 # WALKING
