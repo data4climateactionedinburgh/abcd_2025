8bcccfd9930f4a042b581c331cb239ed3f0cd877
D4CAE <data4climateactionedinburgh@gmail.com>
2025-03-31 20:39:34 +0100

    added metadata of netcdf files for documentation and learning purposes

--- code/temperature.R
+++ code/temperature.R
@@ -35,6 +35,7 @@ get_tas_from_file <- function(filename, single_location_latlong){
   # print(c("Num of dimensions: ", file.inq.nc(nc_conn)["ndims"]))
   # print(c("Num of global attributes: ", file.inq.nc(nc_conn)["ngatts"]))
   # print(c("Num of variables: ", file.inq.nc(nc_conn)["nvars"]))
+  # print.nc(nc_conn)
   tas_from_file <- tibble()
   #tas_from_file$tas  <-  ncvar_get(nc_conn, varid = "tas")
   #tas_from_file$time  <- ncvar_get(nc_conn, varid = "time")
@@ -68,3 +69,153 @@ for (i in 1:length(data_files_months_temprtr)){ #should be 12(!)
 temperature_data_csv_filename <- "my_csv_temperature.csv"
 write.csv2(temperature_to_plot, temperature_data_csv_filename)
 plot(temperature_to_plot)
+
+
+
+# Appendix: Metadata - output from print.nc on annual data file
+# # > print.nc(nc_conn)
+# netcdf netcdf4 {
+#   dimensions:
+#     time = 1 ;
+#     projection_y_coordinate = 1450 ;
+#     projection_x_coordinate = 900 ;
+#     bnds = 2 ;
+#     variables:
+#       NC_DOUBLE tas(projection_x_coordinate, projection_y_coordinate, time) ;
+#     NC_DOUBLE tas:_FillValue = 1e+20 ;
+#     NC_CHAR tas:standard_name = "air_temperature" ;
+#     NC_CHAR tas:long_name = "Mean air temperature" ;
+#     NC_CHAR tas:units = "degC" ;
+#     NC_CHAR tas:description = "Mean air temperature" ;
+#     NC_STRING tas:label_units = "°C" ;
+#     NC_CHAR tas:level = "1.5m" ;
+#     NC_STRING tas:plot_label = "Mean air temperature at 1.5m (°C)" ;
+#     NC_CHAR tas:cell_methods = "time: mid_range within days time: mean over days" ;
+#     NC_CHAR tas:grid_mapping = "transverse_mercator" ;
+#     NC_CHAR tas:coordinates = "latitude longitude" ;
+#     NC_INT transverse_mercator ;
+#     NC_CHAR transverse_mercator:grid_mapping_name = "transverse_mercator" ;
+#     NC_DOUBLE transverse_mercator:longitude_of_prime_meridian = 0 ;
+#     NC_DOUBLE transverse_mercator:semi_major_axis = 6377563.396 ;
+#     NC_DOUBLE transverse_mercator:semi_minor_axis = 6356256.909 ;
+#     NC_DOUBLE transverse_mercator:longitude_of_central_meridian = -2 ;
+#     NC_DOUBLE transverse_mercator:latitude_of_projection_origin = 49 ;
+#     NC_DOUBLE transverse_mercator:false_easting = 4e+05 ;
+#     NC_DOUBLE transverse_mercator:false_northing = -1e+05 ;
+#     NC_DOUBLE transverse_mercator:scale_factor_at_central_meridian = 0.9996012717 ;
+#     NC_INT64 time(time) ;
+#     NC_CHAR time:axis = "T" ;
+#     NC_CHAR time:bounds = "time_bnds" ;
+#     NC_CHAR time:units = "hours since 1800-01-01 00:00:00" ;
+#     NC_CHAR time:standard_name = "time" ;
+#     NC_CHAR time:calendar = "standard" ;
+#     NC_INT64 time_bnds(bnds, time) ;
+#     NC_DOUBLE projection_y_coordinate(projection_y_coordinate) ;
+#     NC_CHAR projection_y_coordinate:axis = "Y" ;
+#     NC_CHAR projection_y_coordinate:bounds = "projection_y_coordinate_bnds" ;
+#     NC_CHAR projection_y_coordinate:units = "m" ;
+#     NC_CHAR projection_y_coordinate:standard_name = "projection_y_coordinate" ;
+#     NC_DOUBLE projection_y_coordinate_bnds(bnds, projection_y_coordinate) ;
+#     NC_DOUBLE projection_x_coordinate(projection_x_coordinate) ;
+#     NC_CHAR projection_x_coordinate:axis = "X" ;
+#     NC_CHAR projection_x_coordinate:bounds = "projection_x_coordinate_bnds" ;
+#     NC_CHAR projection_x_coordinate:units = "m" ;
+#     NC_CHAR projection_x_coordinate:standard_name = "projection_x_coordinate" ;
+#     NC_DOUBLE projection_x_coordinate_bnds(bnds, projection_x_coordinate) ;
+#     NC_DOUBLE latitude(projection_x_coordinate, projection_y_coordinate) ;
+#     NC_CHAR latitude:units = "degrees_north" ;
+#     NC_CHAR latitude:standard_name = "latitude" ;
+#     NC_DOUBLE longitude(projection_x_coordinate, projection_y_coordinate) ;
+#     NC_CHAR longitude:units = "degrees_east" ;
+#     NC_CHAR longitude:standard_name = "longitude" ;
+# 
+#     // global attributes:
+#       NC_CHAR :comment = "Annual resolution gridded climate observations" ;
+#       NC_CHAR :creation_date = "2024-05-17T09:50:50" ;
+#       NC_CHAR :frequency = "ann" ;
+#       NC_CHAR :institution = "Met Office" ;
+#       NC_CHAR :references = "doi: 10.1002/gdj3.78" ;
+#       NC_CHAR :short_name = "annual_meantemp" ;
+#       NC_CHAR :source = "HadUK-Grid_v1.3.0.0" ;
+#       NC_CHAR :title = "Gridded surface climate observations data for the UK" ;
+#       NC_CHAR :version = "v20240514" ;
+#       NC_CHAR :Conventions = "CF-1.7" ;
+# }
+# 
+# ## monthly
+# > nc_conn <- open.nc(here(data_dir_name, data_subdir, data_files_months_temprtr[7L]))
+# > print.nc(nc_conn)
+# netcdf netcdf4 {
+#   dimensions:
+#     time = 1 ;
+#     projection_y_coordinate = 1450 ;
+#     projection_x_coordinate = 900 ;
+#     bnds = 2 ;
+#     variables:
+#       NC_DOUBLE tas(projection_x_coordinate, projection_y_coordinate, time) ;
+#     NC_DOUBLE tas:_FillValue = 1e+20 ;
+#     NC_INT64 tas:least_significant_digit = 3 ;
+#     NC_CHAR tas:standard_name = "air_temperature" ;
+#     NC_CHAR tas:long_name = "Mean air temperature" ;
+#     NC_CHAR tas:units = "degC" ;
+#     NC_CHAR tas:description = "Mean air temperature" ;
+#     NC_STRING tas:label_units = "°C" ;
+#     NC_CHAR tas:level = "1.5m" ;
+#     NC_STRING tas:plot_label = "Mean air temperature at 1.5m (°C)" ;
+#     NC_CHAR tas:cell_methods = "time: mid_range within days time: mean over days" ;
+#     NC_CHAR tas:grid_mapping = "transverse_mercator" ;
+#     NC_CHAR tas:coordinates = "latitude longitude month_number season_year" ;
+#     NC_INT transverse_mercator ;
+#     NC_CHAR transverse_mercator:grid_mapping_name = "transverse_mercator" ;
+#     NC_DOUBLE transverse_mercator:longitude_of_prime_meridian = 0 ;
+#     NC_DOUBLE transverse_mercator:semi_major_axis = 6377563.396 ;
+#     NC_DOUBLE transverse_mercator:semi_minor_axis = 6356256.909 ;
+#     NC_DOUBLE transverse_mercator:longitude_of_central_meridian = -2 ;
+#     NC_DOUBLE transverse_mercator:latitude_of_projection_origin = 49 ;
+#     NC_DOUBLE transverse_mercator:false_easting = 4e+05 ;
+#     NC_DOUBLE transverse_mercator:false_northing = -1e+05 ;
+#     NC_DOUBLE transverse_mercator:scale_factor_at_central_meridian = 0.9996012717 ;
+#     NC_INT64 time(time) ;
+#     NC_CHAR time:axis = "T" ;
+#     NC_CHAR time:bounds = "time_bnds" ;
+#     NC_CHAR time:units = "hours since 1800-01-01 00:00:00" ;
+#     NC_CHAR time:standard_name = "time" ;
+#     NC_CHAR time:calendar = "standard" ;
+#     NC_INT64 time_bnds(bnds, time) ;
+#     NC_DOUBLE projection_y_coordinate(projection_y_coordinate) ;
+#     NC_CHAR projection_y_coordinate:axis = "Y" ;
+#     NC_CHAR projection_y_coordinate:bounds = "projection_y_coordinate_bnds" ;
+#     NC_CHAR projection_y_coordinate:units = "m" ;
+#     NC_CHAR projection_y_coordinate:standard_name = "projection_y_coordinate" ;
+#     NC_DOUBLE projection_y_coordinate_bnds(bnds, projection_y_coordinate) ;
+#     NC_DOUBLE projection_x_coordinate(projection_x_coordinate) ;
+#     NC_CHAR projection_x_coordinate:axis = "X" ;
+#     NC_CHAR projection_x_coordinate:bounds = "projection_x_coordinate_bnds" ;
+#     NC_CHAR projection_x_coordinate:units = "m" ;
+#     NC_CHAR projection_x_coordinate:standard_name = "projection_x_coordinate" ;
+#     NC_DOUBLE projection_x_coordinate_bnds(bnds, projection_x_coordinate) ;
+#     NC_DOUBLE latitude(projection_x_coordinate, projection_y_coordinate) ;
+#     NC_CHAR latitude:units = "degrees_north" ;
+#     NC_CHAR latitude:standard_name = "latitude" ;
+#     NC_DOUBLE longitude(projection_x_coordinate, projection_y_coordinate) ;
+#     NC_CHAR longitude:units = "degrees_east" ;
+#     NC_CHAR longitude:standard_name = "longitude" ;
+#     NC_INT64 month_number ;
+#     NC_CHAR month_number:units = "1" ;
+#     NC_CHAR month_number:long_name = "month_number" ;
+#     NC_INT64 season_year ;
+#     NC_CHAR season_year:units = "1" ;
+#     NC_CHAR season_year:long_name = "season_year" ;
+#     
+#     // global attributes:
+#       NC_CHAR :comment = "Monthly resolution gridded climate observations" ;
+#       NC_CHAR :creation_date = "2024-08-06T10:07:26" ;
+#       NC_CHAR :frequency = "mon" ;
+#       NC_CHAR :institution = "Met Office" ;
+#       NC_CHAR :references = "doi: 10.1002/gdj3.78" ;
+#       NC_CHAR :short_name = "monthly_meantemp" ;
+#       NC_CHAR :source = "HadUK-Grid_Provisional" ;
+#       NC_CHAR :title = "Gridded surface climate observations data for the UK" ;
+#       NC_CHAR :version = "provisional" ;
+#       NC_CHAR :Conventions = "CF-1.7" ;
+# }
